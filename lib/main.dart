fea0355fa0ddefe91bde61730728194eecad982e
Phillip LeMaster <philalem21@gmail.com>
2020-07-14 11:09:25 -0400

    added daily notifications

--- lib/main.dart
+++ lib/main.dart
@@ -2,21 +2,76 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:hydration_tracker/existingUserWrapper.dart';
+import 'package:rxdart/subjects.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
+final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+    FlutterLocalNotificationsPlugin();
+
+// Streams are created so that app can respond to notification-related events since the plugin is initialised in the `main` function
+final BehaviorSubject<ReceivedNotification> didReceiveLocalNotificationSubject =
+    BehaviorSubject<ReceivedNotification>();
+
+final BehaviorSubject<String> selectNotificationSubject =
+    BehaviorSubject<String>();
+
+NotificationAppLaunchDetails notificationAppLaunchDetails;
+
+class ReceivedNotification {
+  final int id;
+  final String title;
+  final String body;
+  final String payload;
+
+  ReceivedNotification({
+    @required this.id,
+    @required this.title,
+    @required this.body,
+    @required this.payload,
+  });
+}
+
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   scheduleNotificationsForTheWeek();
   runApp(HydrationTracker());
 }
 
 Future scheduleNotificationsForTheWeek() async {
-  if (await checkIfAWeekHasPassed() == 0) {
+  notificationAppLaunchDetails =
+      await flutterLocalNotificationsPlugin.getNotificationAppLaunchDetails();
+
+  var initializationSettingsAndroid = AndroidInitializationSettings('app_icon');
+  // Note: permissions aren't requested here just to demonstrate that can be done later using the `requestPermissions()` method
+  // of the `IOSFlutterLocalNotificationsPlugin` class
+  var initializationSettingsIOS = IOSInitializationSettings(
+      requestAlertPermission: true,
+      requestBadgePermission: true,
+      requestSoundPermission: true,
+      onDidReceiveLocalNotification:
+          (int id, String title, String body, String payload) async {
+        didReceiveLocalNotificationSubject.add(ReceivedNotification(
+            id: id, title: title, body: body, payload: payload));
+      });
+  var initializationSettings = InitializationSettings(
+      initializationSettingsAndroid, initializationSettingsIOS);
+  await flutterLocalNotificationsPlugin.initialize(initializationSettings,
+      onSelectNotification: (String payload) async {
+    if (payload != null) {
+      debugPrint('notification payload: ' + payload);
+    }
+    selectNotificationSubject.add(payload);
+  });
+  SharedPreferences preferences = await SharedPreferences.getInstance();
+  DateTime today = DateTime.now();
+  if (!await checkIfAWeekHasPassed(preferences, today)) {
     return;
   }
-  for (int i = 0; i < 7; i++) {
-    FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
-        FlutterLocalNotificationsPlugin();
-    var scheduledNotificationDateTime = DateTime.now().add(Duration(days: i));
+  preferences.setString(
+      'lastDateToScheduleNotifications', today.toIso8601String());
+  for (int i = 1; i < 8; i++) {
+    var scheduledNotificationDateTime =
+        DateTime(today.year, today.month, today.day + i, 6, 30);
     var androidPlatformChannelSpecifics = AndroidNotificationDetails(
         'Hydrate',
         'Hydration App',
@@ -33,17 +88,16 @@ Future scheduleNotificationsForTheWeek() async {
   }
 }
 
-Future<int> checkIfAWeekHasPassed() async {
-  SharedPreferences preferences = await SharedPreferences.getInstance();
+Future<bool> checkIfAWeekHasPassed(
+    SharedPreferences preferences, DateTime today) async {
   String lastScheduledDate =
       preferences.getString('lastDateToScheduleNotifications');
-  DateTime today = DateTime.now();
+  if (lastScheduledDate == null) {
+    return true;
+  }
   DateTime storedDate =
       DateTime.parse(restrictFractionalSeconds(lastScheduledDate));
-  if (storedDate == null) {
-    return 1;
-  }
-  return storedDate.difference(today).inDays == 7 ? 1 : 0;
+  return storedDate.difference(today).inDays >= 7;
 }
 
 String restrictFractionalSeconds(String dateTime) =>
