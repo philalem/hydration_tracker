4fe169141ba8b183d602c41d53501687c244e645
Phillip LeMaster <philalem21@gmail.com>
2020-07-15 11:10:11 -0400

    added only portrait mode

--- lib/main.dart
+++ lib/main.dart
@@ -115,6 +115,10 @@ class Hydratee extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+    ]);
     return CupertinoApp(
       title: 'Flutter Demo',
       theme: CupertinoThemeData(
